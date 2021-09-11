71ad1a8849813d3fd851439d0f2cd57b8309f8af
Nick Alvarez <48071045+nicky189@users.noreply.github.com>
2021-09-11 15:45:13 -0700

    Min distance works. Added progressbar This thing takes forever to run.

--- Project 1/p1_1.py
+++ Project 1/p1_1.py
@@ -2,18 +2,14 @@ import numpy as np
 from scipy.spatial import ConvexHull
 from scipy.spatial.distance import cdist
 from matplotlib import pyplot as plt
+from progressbar import progressbar
 
-# Something like...
-#numpy.random.uniform(0.0, 1.0, (1000))
-#numpy.random.uniform(0.0, 1.0, (1000,2))
-#numpy.random.uniform(0.0, 1.0, (1000,3,3))
-#numpy.random.uniform(0.0, 1.0, (1000,4,4,4))
-#numpy.random.uniform(0.0, 1.0, (1000,5,5,5,5))
-# Nope lol nvm.. np.random.rand(N,d)
+MAX_ITERATIONS = 15
+NUMBER_OF_POINTS = 100
 
 ## 1000 points under one dimension
-points = np.random.rand(10,3)
-print(points)
+#points = np.random.rand(10,2)
+#print(points)
 
 def findMinMax(p):
   #d = np.diff(p, axis=0)
@@ -22,43 +18,41 @@ def findMinMax(p):
   #print(max(segmentDistances)) #Wrong
   #print(min(segmentDistances))
 
-  hull = ConvexHull(points)
+  hull = ConvexHull(p)
   # Extract the points forming the hull
-  hullpoints = points[hull.vertices,:]
+  hullpoints = p[hull.vertices,:]
   # Naive way of finding the best pair in O(H^2) time if H is number of points on hull
   hdist = cdist(hullpoints, hullpoints, metric='euclidean')
   # Get the farthest apart points
   maxpair = np.unravel_index(hdist.argmax(), hdist.shape)
-  minpair = np.unravel_index(hdist.argmin(), hdist.shape)
+  i,j = np.where(hdist==np.min(hdist[np.nonzero(hdist)]))
 
-  print(hullpoints[maxpair[0]], hullpoints[maxpair[1]])
-  print(hullpoints[minpair[0]], hullpoints[minpair[1]])
+  #print(hullpoints[maxpair[0]], hullpoints[maxpair[1]])
+  #print(hullpoints[i][0], hullpoints[i][1])
 
-
-  #print([hullpoints[bestpair[0]],hullpoints[bestpair[1]]])
-  #print(np.linalg.norm(hullpoints[bestpair[0]] - hullpoints[bestpair[1]]))
-
-  pairPoints = np.array([(np.linalg.norm(hullpoints[minpair[0]] - hullpoints[minpair[1]])),(np.linalg.norm(hullpoints[maxpair[0]] - hullpoints[maxpair[1]]))])
-  print(pairPoints)
+  pairPoints = np.array([(np.linalg.norm(hullpoints[i][0] - hullpoints[i][1])),(np.linalg.norm(hullpoints[maxpair[0]] - hullpoints[maxpair[1]]))])
+  #print(pairPoints)
 
   logDiff = np.log10((pairPoints[1]-pairPoints[0])/pairPoints[0])
   print(logDiff)
-  #listOfLogDiffs.append(logDiff)
-
-findMinMax(points)
-
-listOfLogDiffs = []
-
-#for x in range(2,3):
-#  findMinMax(np.random.rand(1000,x))
-#print(listOfLogDiffs)
+  listOfLogDiffs.append(logDiff)
 
 ## Plotting the log results
 def plotGraph():
-  x = np.arange(2,51)
+  x = np.arange(2,MAX_ITERATIONS)
   y = listOfLogDiffs
   plt.title("Curse of Dimensionality") 
   plt.xlabel("Number of Dimensions") 
   plt.ylabel("Log_10 * ((Max-Min)/Min)") 
   plt.plot(x,y) 
   plt.show()
+
+listOfLogDiffs = []
+
+for x in progressbar(range(2,MAX_ITERATIONS), redirect_stdout=True):
+#for x in range(2,MAX_ITERATIONS):
+  print(f"({x}/{MAX_ITERATIONS}): ", end='')
+  findMinMax(np.random.rand(NUMBER_OF_POINTS,x))
+#print(listOfLogDiffs)
+
+plotGraph()

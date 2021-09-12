12cec85e03ffc864eb323d88f6d54f2898683f58
Nick Alvarez <48071045+nicky189@users.noreply.github.com>
2021-09-12 13:41:12 -0700

    Add non-functional code to average points

--- Project 1/p1_1.py
+++ Project 1/p1_1.py
@@ -4,35 +4,30 @@ from scipy.spatial.distance import cdist
 from matplotlib import pyplot as plt
 from progressbar import progressbar
 
-MAX_ITERATIONS = 15
-NUMBER_OF_POINTS = 100
+MAX_ITERATIONS = 10
+NUMBER_OF_POINTS = 50
 
-## 1000 points under one dimension
-#points = np.random.rand(10,2)
+## 1000 points under two dimension
+#points = np.random.rand(1000,2)
 #print(points)
 
 def findMinMax(p):
-  #d = np.diff(p, axis=0)
-  #print(d)
-  #segmentDistances = np.hypot(d[:,0], d[:,1]) #Wrong
-  #print(max(segmentDistances)) #Wrong
-  #print(min(segmentDistances))
-
   hull = ConvexHull(p)
   # Extract the points forming the hull
   hullpoints = p[hull.vertices,:]
   # Naive way of finding the best pair in O(H^2) time if H is number of points on hull
   hdist = cdist(hullpoints, hullpoints, metric='euclidean')
-  # Get the farthest apart points
+  
+  ### This code performs as well as cdist. distance.euclidian failed me
+    #b = hullpoints.reshape(hullpoints.shape[0], 1, hullpoints.shape[1])
+    #hdist = np.sqrt(np.einsum('ijk, ijk->ij', hullpoints-b, hullpoints-b))
+  #######
+
+  # Get the min/max points
   maxpair = np.unravel_index(hdist.argmax(), hdist.shape)
   i,j = np.where(hdist==np.min(hdist[np.nonzero(hdist)]))
 
-  #print(hullpoints[maxpair[0]], hullpoints[maxpair[1]])
-  #print(hullpoints[i][0], hullpoints[i][1])
-
   pairPoints = np.array([(np.linalg.norm(hullpoints[i][0] - hullpoints[i][1])),(np.linalg.norm(hullpoints[maxpair[0]] - hullpoints[maxpair[1]]))])
-  #print(pairPoints)
-
   logDiff = np.log10((pairPoints[1]-pairPoints[0])/pairPoints[0])
   print(logDiff)
   listOfLogDiffs.append(logDiff)
@@ -48,11 +43,24 @@ def plotGraph():
   plt.show()
 
 listOfLogDiffs = []
+#bigList = []
+#averagePoints = []
 
+#for y in progressbar(range(2,MAX_ITERATIONS), redirect_stdout=True):
+#  listOfLogDiffs = []
 for x in progressbar(range(2,MAX_ITERATIONS), redirect_stdout=True):
-#for x in range(2,MAX_ITERATIONS):
   print(f"({x}/{MAX_ITERATIONS}): ", end='')
   findMinMax(np.random.rand(NUMBER_OF_POINTS,x))
+  #bigList.append(listOfLogDiffs)
+
+
+## Multiple Runs // Average the points for each dimension
 #print(listOfLogDiffs)
+#for x in range(0, len(bigList)):
+#  pointsToSum = 0.0
+#  for y in range(0, MAX_ITERATIONS):
+#    pointsToSum += (bigList[y][x])
+# averagePoints.append(float(pointsToSum/MAX_ITERATIONS))
+  
 
 plotGraph()

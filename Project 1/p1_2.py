a15df3df97dcdedd00a1ca78a3152036627b8395
Nick Alvarez <nickalvarez189@gmail.com>
2021-09-16 21:39:54 -0700

    Plot rectangles around centroids and add legend

--- Project 1/p1_2.py
+++ Project 1/p1_2.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import numpy as np
 from sklearn import datasets
 from sklearn.cluster import KMeans
@@ -13,11 +14,13 @@ y = iris.target
 MarkerList = ['s', 'o', 'd']
 TitleList = ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width"]
 
-
+# Create Figure 1
 fig, axs = plt.subplots(NUMBER_PLOTS_SQ, NUMBER_PLOTS_SQ)
 fig.suptitle("Iris Data (blue=setosa, red=versicolor, green=virginica)")
+# Create Figure 2
 fig2, axs2 = plt.subplots()
 
+# Iris 16 plots
 def generatePlots():
   for v in range(NUMBER_PLOTS_SQ):
     for h in range(NUMBER_PLOTS_SQ):
@@ -29,19 +32,47 @@ def generatePlots():
         axs[v][h].yaxis.set_visible(False)
 
 def generateDiscretization():
+  # Scatter two variables
+  IrisLabels = ['Setosa', 'Versicolor', 'Virginica']
   for i in range(np.prod(y.shape)):
     axs2.scatter(x[i, 2], x[i, 3], marker=MarkerList[int(i/50)], c='k', s=10.0)
+  #axs2.set_label(['Setosa', 'Versicolor', 'Virginica'])
+  for l in range(len(IrisLabels)):
+    axs2.scatter([],[],color='k', label=IrisLabels[l], marker=MarkerList[l])
+  axs2.legend()
   
+  # Delete unnecessary variables
   ClusterArray = np.delete(x, [0,1], 1)
-  print(ClusterArray)
 
-  axs2.set_xlabel("Petal Length")
-  axs2.set_ylabel("Petal Width")
+  # Generate clusters
+  axs2.set_xlabel("Petal Length (cm)")
+  axs2.set_ylabel("Petal Width (cm)")
   km = KMeans(n_clusters=3)
   km.fit(ClusterArray)
 
+  # Find centroids and plot them
   km_cntr = km.cluster_centers_
   axs2.scatter(km_cntr[:, 0], km_cntr[:, 1], c='red', s=15.0)
+  
+  # Create rectangles around clusters
+  CentroidRectangleLengths = np.empty((0,2))
+  for i in range(0,101,50):
+    try:
+      val_min_x, val_max_x = np.min(np.delete(ClusterArray[i:(i+50)], 1, 1), axis=0), np.max(np.delete(ClusterArray[i:(i+50)], 1, 1), axis=0)
+      val_min_y, val_max_y = np.min(np.delete(ClusterArray[i:(i+50)], 0, 1), axis=0), np.max(np.delete(ClusterArray[i:(i+50)], 0, 1), axis=0)
+      x_len, y_len = val_max_x-val_min_x, val_max_y-val_min_y
+      CentroidRectangleLengths = np.append(CentroidRectangleLengths, [np.concatenate((x_len, y_len))], axis=0)
+    except ValueError:
+      pass
+  
+  # Plot the rectangles
+  index=0
+  sortedCenters = np.sort(km_cntr, axis=0)
+  for c in (sortedCenters):
+    width, height = CentroidRectangleLengths[index][0], CentroidRectangleLengths[index][1]
+    axs2.add_patch(Rectangle(
+      xy=(c[0]-width/2, c[1]-height/2), width=width, height=height, linewidth=1, color='black', fill=False))
+    index += 1
 
 generatePlots() #(a)
 generateDiscretization() #(b)

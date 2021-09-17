b12e94f5a1cc202b6cdbf439a23df81491b4761e
Nick Alvarez <nickalvarez189@gmail.com>
2021-09-16 23:06:39 -0700

    Add legend and color

--- Project 1/p1_3.py
+++ Project 1/p1_3.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn import datasets
@@ -7,13 +8,15 @@ from sklearn import datasets
 iris = datasets.load_iris()
 x = iris.data
 y = iris.target
+IrisLabels = ['Versicolor', 'Setosa', 'Virginica']
+PlotColors = ['r', 'b', 'g']
 
 fig, axs = plt.subplots()
 fig2, axs2 = plt.subplots()
 
 # No decomposition
 def generateBasePlot():
-  axs.scatter(x[:, 0], x[:, 1])
+  axs.scatter(x[:, 0], x[:, 1], c=y, cmap=plt.cm.brg)
   axs.set_xlabel("Sepal Length (cm)")
   axs.set_ylabel("Sepal width (cm)")
 
@@ -22,10 +25,18 @@ def generatePCAPlot():
   pca_iris = PCA(3)
   pca_iris.fit(x)
   decomp_x = pca_iris.transform(x)
-  axs2.scatter(decomp_x[:,0], decomp_x[:,1])
+  axs2.scatter(decomp_x[:,0], decomp_x[:,1], c=y, cmap=plt.cm.brg)
   axs2.set_xlabel("Sepal Length (cm)")
   axs2.set_ylabel("Sepal width (cm)")
 
 generateBasePlot()
 generatePCAPlot()
+
+leg = []
+for l in range(len(IrisLabels)):
+  leg.append(Rectangle((0,0),1,1,fc=PlotColors[l]))
+axs.legend(leg, IrisLabels)
+axs2.legend(leg, IrisLabels)
+
+
 plt.show()

f79a4d0b4606efcc956da94e9ad75552f6929394
Nick Alvarez <48071045+nicky189@users.noreply.github.com>
2021-11-20 19:35:32 -0800

    Add regression model

--- Final Project/Final.py
+++ Final Project/Final.py
@@ -1,15 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import svm, metrics
 from Class_Solar import Solar
+
 NUMBER_ATTRIBUTES = 15
 BEGIN_AT = 0
-s_train = Solar("solar_training.csv")
+_VALUETOTEST = 8
+_NUMBERZONES = 3
+np.set_printoptions(precision=5, suppress=True)
 
-fig, axs = plt.subplots(NUMBER_ATTRIBUTES, NUMBER_ATTRIBUTES)
-fig.tight_layout()
-#plt.scatter(s_train.var78, s_train.power, s=0.1)
+s_train = Solar("solar_training.csv")
+print("Shape of training data:", s_train.data.shape)
+s_test = Solar("solar_test.csv")#, 48159)
+print("Shape of test data:", s_test.data.shape)
+
+RMSE_Scores = [0,0,0]
+MAE_Scores = [0,0,0]
+
+def truncate(num, digits):
+  l = str(float(num)).split('.')
+  digits = min(len(l[1]), digits)
+  return l[0] + '.' + l[1][:digits]
 
 def generatePlots():
+  fig, axs = plt.subplots(NUMBER_ATTRIBUTES, NUMBER_ATTRIBUTES)
+  fig.tight_layout()
+  plt.scatter(s_train.var78, s_train.power, s=0.1)
+
   attributes = s_train.getAttributes()
   names = s_train.getNames()
   coef = s_train.correlation()
@@ -31,7 +48,39 @@ def generatePlots():
         axs[v][h].xaxis.set_visible(False)
         axs[v][h].yaxis.set_visible(False)
 
-generatePlots()
-plt.show()
+def makeModel():
+  model = svm.SVR()
+  model.fit(s_train.data, s_train.power)
+  y_pred_train = model.predict(s_train.data)
+  #print(f"RMSE with training data: {metrics.mean_squared_error(s_train.power, y_pred_train, squared=False)}%")
+  return model
 
-#s_train.correlation()
+def runModel():
+  for z in range(0, _NUMBERZONES):
+    y_pred_test = regr.predict(s_test.zonedata[z])
+    #print(f"RMSE of test data on Zone {z}: {metrics.mean_squared_error(s_test.zonepower[z], y_pred_test, squared=False)}%")
+    RMSE_Scores[z] = metrics.mean_squared_error(s_test.zonepower[z], y_pred_test, squared=False)
+    MAE_Scores[z] = metrics.mean_absolute_error(s_test.zonepower[z], y_pred_test)
+
+def printScores():
+  RMSE_out = str()
+  MAE_out = str()
+  RMSE_avg = 0
+  MAE_avg = 0
+  for i in range(3):
+    RMSE_out += ("\t" + truncate(RMSE_Scores[i], 6))
+    RMSE_avg += RMSE_Scores[i]
+    MAE_out += ("\t" + truncate(MAE_Scores[i], 6))
+    MAE_avg += MAE_Scores[i]
+  RMSE_out += ("\t" + truncate(RMSE_avg/3.0, 6))
+  MAE_out += ("\t" + truncate(MAE_avg/3.0, 6))
+
+  print("\n\t\t###### Scoring Metrics ######")
+  print("\tZone 1\t\tZone 2\t\tZone 3\t\tOverall")
+  print(f"RMSE{RMSE_out}")
+  print(f"MAE{MAE_out}")
+  
+
+regr = makeModel()
+runModel()
+printScores()
